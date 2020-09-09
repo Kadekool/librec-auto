f50348a546ccf7904ced0de169fee357c098fa63
walterscorpionblade <36717405+walterscorpionblade@users.noreply.github.com>
2020-09-09 02:08:16 -0600

    box-plot error

--- librec_auto/cmd/post/result-graphics.py
+++ librec_auto/cmd/post/result-graphics.py
@@ -1,4 +1,3 @@
-
 import argparse
 from librec_auto import read_config_file
 from librec_auto.util import Status
@@ -9,12 +8,59 @@ import matplotlib.pyplot as plt
 
 matplotlib.use('Agg')  # For non-windowed plotting
 
-viz_file_pattern_bar = "viz-bar-{}.jpg"
-viz_file_pattern_box = "viz-box-{}.jpg"
+viz_file_pattern_bar = "viz-bar-{}.png"
+viz_file_pattern_box = "viz-box-{}.png"
 viz_html_filename = "viz.html"
 exp_dir_pattern = "exp[0-9][0-9][0-9]"
 
+#This is slack library
+import sys
+import slack as sl
+from slacker import Slacker
 
+#This is dropbox library
+import dropbox
+import sys, os
+
+#This is decrypted function
+from cryptography.fernet import Fernet
+
+'''
+Zijun:
+    Arg: 
+    1). message: information wants to put into the slack 
+    2). slack_api: slack group channel api key
+    3). channel: which channel wants to put 
+'''
+def slack_send_message(message, slack_api, channel):
+    slack = Slacker(str(slack_api))
+    slack.chat.post_message(channel, message)
+
+#Zijun: This is function for auto send file and comments to slack
+''''
+Zijun:
+    Arg:
+    1). rep: repository for the file
+    2). fil1: filename
+    3). tit1: title name
+    4). slack_api: slack group channel api key
+    5). channel: which channel wants to put 
+'''
+def slack_send_file(rep, fil1, tit1, slack_api, channel):
+    client = sl.WebClient(token=slack_api)
+    with open(rep, 'rb') as att:
+        r = client.api_call("files.upload", files={'file': att,},
+                            data={'channels': channel, 'filename': fil1, 'title': tit1, 'initial_comment': 'This is {}'.format(fil1),}
+                            )
+    assert r.status_code == 200
+
+
+# #Zijun: This is function for auto uploading the file into dropbox
+def dropbox_send_file(rep, file_name, dropbox_api_2):
+    dropbox_api = dropbox.Dropbox(dropbox_api_2)
+    with open(rep, "rb") as f:
+        dropbox_api.files_upload(f.read(), file_name, mute=True)
+    print ("Done uploading plot")
 # Things we need from each experiment run
 # name (dir), params that changed, values of changing parameters, metrics measured,
 
@@ -35,7 +81,7 @@ def get_metric_info(files):
     return metric_info
 
 
-def create_bar(path, metric_name, params, settings, metric_values):
+def create_bar(path, metric_name, params, settings, metric_values, slack_api, slack_api_2, channel):
     x_range = range(0, len(settings))
 
     fig, ax = plt.subplots()
@@ -48,15 +94,15 @@ def create_bar(path, metric_name, params, settings, metric_values):
     filename = path / viz_file_pattern_bar.format(metric_name)
     print(str(filename))
     fig.savefig(str(filename))
+    slack_send_file(filename, viz_file_pattern_bar.format(metric_name), viz_file_pattern_bar.format(metric_name), slack_api, channel)
+    dropbox_send_file(filename, "/Librec-auto/"+viz_file_pattern_bar.format(metric_name), slack_api_2)
     plt.close()
-
     return filename
 
 
-def create_bars(path, metric_info):
+def create_bars(path, metric_info, slack_api, slack_api_2, channel):
     metric_names = list(metric_info.values())[0][2].get_metrics()
     # Nasim: add list to it because in python 3 it returns a view, so it doesn't have indexing, you can't access it.
-
     bar_paths = []
 
     for metric in metric_names:
@@ -70,12 +116,12 @@ def create_bars(path, metric_info):
             settings.append('\n'.join(vals))
             metric_vals.append(float(log.get_metric_values(metric)[-1]))
 
-        bar_paths.append(create_bar(path, metric, param_string, settings, metric_vals))
+        bar_paths.append(create_bar(path, metric, param_string, settings, metric_vals, slack_api, slack_api_2, channel))
 
     return bar_paths
 
 
-def create_box(path, metric, params, settings, fold_values):
+def create_box(path, metric, params, settings, fold_values, slack_api, slack_api_2, channel):
     fig, ax = plt.subplots()
     ax.boxplot(fold_values)
     ax.set_ylabel(metric)
@@ -83,13 +129,15 @@ def create_box(path, metric, params, settings, fold_values):
     ax.set_title('{} distribution by\n{}'.format(metric, params))
 
     filename = path / viz_file_pattern_box.format(metric)
-
+    print (filename)
     fig.savefig(str(filename))
+    slack_send_file(filename, viz_file_pattern_box.format(metric), viz_file_pattern_box.format(metric), slack_api, channel)
+    dropbox_send_file(filename, "/Librec-auto/"+viz_file_pattern_box.format(metric), slack_api_2)
     plt.close()
     return filename
 
 
-def create_boxes(path, metric_info):
+def create_boxes(path, metric_info, slack_api, slack_api_2, channel):
     metric_names = list(metric_info.values())[0][2].get_metrics()
     print(metric_names)
 
@@ -106,7 +154,7 @@ def create_boxes(path, metric_info):
             metric_vals = log.get_metric_values(metric)[:-1]
             fold_vals.append([float(val) for val in metric_vals])
 
-        box_paths.append(create_box(path, metric, param_string, settings, fold_vals))
+        box_paths.append(create_box(path, metric, param_string, settings, fold_vals, slack_api, slack_api_2, channel))
 
     return box_paths
 
@@ -138,14 +186,14 @@ def create_html(path, metric_info, bars, boxes):
     return filename
 
 
-def create_graphics(config, display):
+def create_graphics(config, display, slack_api, slack_api_2, channel):
     files = config.get_files()
     metric_info = get_metric_info(config.get_files())
 
-    bars = create_bars(files.get_post_path(), metric_info)
+    bars = create_bars(files.get_post_path(), metric_info, slack_api, slack_api_2, channel)
 
     if 'data.splitter.cv.number' in config.get_prop_dict():         # Box plot only makes sense for cross-validation
-        boxes = create_boxes(files.get_post_path(), metric_info)
+        boxes = create_boxes(files.get_post_path(), metric_info, slack_api, slack_api_2, channel)
     else:
         boxes = None
 
@@ -153,6 +201,29 @@ def create_graphics(config, display):
         html_file = create_html(files.get_post_path(), metric_info, bars, boxes)
         webbrowser.open('file://' + str(html_file.absolute()), new=1, autoraise=True)
 
+def decrypted_function(repo_key1, repo_encrypted_file, repo_decrypted_file):
+    file = open(repo_key1, "rb")
+    key = file.read()
+    file.close()
+
+    with open(repo_encrypted_file, "rb") as api:
+        data = api.read()
+    fernet = Fernet(key)
+    encrypted = fernet.decrypt(data)
+    with open(repo_decrypted_file,"wb") as decry:
+        decry.write(encrypted)
+
+def decrypted_file_Dropbox(repo_key, repo_encrypted_Dropbox_file, repo_decrypted_Dropbox_file):
+    file = open(repo_key, "rb")
+    key = file.read()
+    file.close()
+
+    with open(repo_encrypted_Dropbox_file, "rb") as api:
+        data = api.read()
+    fernet = Fernet(key)
+    encrypted = fernet.decrypt(data)
+    with open(repo_decrypted_Dropbox_file,"wb") as decry:
+        decry.write(encrypted)
 
 def read_args():
     """
@@ -163,7 +234,12 @@ def read_args():
     parser.add_argument('conf', help='Path to configuration file')
     parser.add_argument('target', help='Experiment target')
     parser.add_argument('--browser', help='Show graphics in browser', choices=['True', 'False'])
-
+    parser.add_argument('--channel', help='Input slack channel')
+    parser.add_argument('--decrypted_file', help="repository of decrypted slack api key file")
+    parser.add_argument('--encrypted_file', help="repository of encrpyted slack api key file")
+    parser.add_argument('--key', help="repository of key file")
+    parser.add_argument('--decrypted_Dropbox_file', help="repository of decrypted Dropbox api key file")
+    parser.add_argument('--encrypted_Dropbox_file', help="repository of encrypted Dropbox api key file")
     input_args = parser.parse_args()
     return vars(input_args)
 
@@ -175,5 +251,18 @@ if __name__ == '__main__':
     print(f"librec-auto: Creating summary visualizations for {args['target']}")
 
     display = args['browser'] == 'True'
+    channel = args['channel']
+    decrypted_file = args['decrypted_file']
+    encrypted_file = args['encrypted_file']
+    key_file = args['key']
 
-    create_graphics(config, display)
+    decrypted_Dropbox_file = args['decrypted_Dropbox_file']
+    encrypted_Dropbox_file = args["encrypted_Dropbox_file"]
+
+    decrypted_function(str(key_file), str(encrypted_file), str(decrypted_file))
+    decrypted_file_Dropbox(str(key_file), str(encrypted_Dropbox_file), str(decrypted_Dropbox_file))
+    with open(decrypted_file, "r") as file:
+        slack_api_1 = file.read()
+    with open(decrypted_Dropbox_file, "r") as file:
+        Dropbox_api = file.read()
+    create_graphics(config, display, slack_api_1, Dropbox_api, channel)
